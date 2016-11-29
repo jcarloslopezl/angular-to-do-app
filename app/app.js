6441918c8106f00e4228b84f6cf711368a247c4c
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-11-29 12:27:15 -0600

    Create to-do form

--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@
 
 // app.js
 angular
-		.module('todoApp', ["ngRoute", "ngResource"])
+		.module('todoApp', ["lumx", "ngRoute", "ngResource"])
 		.config(function($routeProvider){
 			$routeProvider
 				.when("/", {
 					controller: "HomeController",
 					templateUrl: "templates/home.html" 
+				})
+				.when("/todo/new",{
+					controller: "NewTodoController",
+					templateUrl: "templates/todo_form.html"
 				});
 		});
--- app/controllers.js
+++ app/controllers.js
@@ -2,11 +2,22 @@
 
 // controllers.js
 angular.module('todoApp')
-  .controller('HomeController', function($scope, TodoResource){
+  .controller('HomeController', function($scope, $location, TodoResource){
   	$scope.title = "Home";
 
 		$scope.todos = [];
 
 		// Get ToDos
 		$scope.todos = TodoResource.query();
+		console.log($scope.todos);
+	})
+	.controller('NewTodoController', function($scope, $location, TodoResource){
+		// Create ToDo		
+		$scope.todo = {};
+		$scope.title = "Create ToDo";
+		$scope.createTodo = function(){
+			TodoResource.save({data: $scope.todo}, function(data){				
+				$location.path("/");
+			});
+		}
 	});
