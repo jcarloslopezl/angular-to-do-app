b59297e97176e13baf44326f73e68cffe416079e
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-12-02 08:07:37 -0600

    Change Api endpoint

--- app/services.js
+++ app/services.js
@@ -3,5 +3,5 @@
 // services.js
 angular.module('todoApp')
   .factory('TodoResource', function($resource){
-  	return $resource("https://bf02b8e1.ngrok.io/:id", { id: "@id" }, { update: { method: "PATCH" }});
+  	return $resource("https://jsonplaceholder.typicode.com/todos/:id", { id: "@id" }, { update: { method: "PATCH" }});
   })
